4596165254a59c703eb01de740e8aef8d17334e5
Razan Srour <razanmsrour@gmail.com>
2024-11-09 12:11:29 +0300

    create testing script

--- test.c
+++ test.c
@@ -1,1 +1,21 @@
+#include "get_next_line.h"
 
+int main()
+{
+    int fd;
+
+    fd = open("test.txt", O_RDONLY);
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+    char *line;
+    while ((line = get_next_line(fd)) != NULL)
+    {
+        printf("Line: %s", line);
+        free(line);
+    }
+    close(fd);
+    return(0);
+}
