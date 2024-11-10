2cb9b9d7e88685b75160d8ca5b116f355c14500e
Razan Srour <razanmsrour@gmail.com>
2024-11-10 19:17:22 +0300

    Fix the ft_strchr to correctly find '\n'

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 18:49:48 by gitpod            #+#    #+#             */
-/*   Updated: 2024/11/09 19:47:00 by rsrour           ###   ########.fr       */
+/*   Updated: 2024/11/10 19:12:38 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,58 +21,48 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	line[fd] = manage_buffer(fd, line);
-	printf("The value is returned to get_next_line");
-	printf("line[fd] = %s\n", line[fd]);
 	if (line[fd] == NULL)
 		return (NULL);
-	printf("line[%d]: %s\n", fd, line[fd]);
-	p_line = get_line(fd, line);
-	return ("Testing");
+	p_line = get_line(line[fd]);
+	return (p_line);
 }
 
 char	*manage_buffer(int fd, char **line)
 {
-	char	*buffer;
 	char	temp[BUFFER_SIZE + 1];
 	ssize_t	read_size;
-	size_t	iter;
 
-	iter = 0;
 	while ((read_size = read(fd, temp, BUFFER_SIZE + 1)) > 0)
 	{
 		temp[read_size] = '\0';
 		if(!line[fd])
 			line[fd] = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		buffer = ft_strjoin(line[fd], temp);
-		if (!buffer)
+		line[fd] = ft_strjoin(line[fd], temp);
+		if (!line[fd])
 			return (NULL);
-		line[fd] = buffer;
-		while (temp[iter] != '\0')
-		{
-			if (temp[iter] == '\n')
-				break;
-			iter++;
-		}
-		printf("line[fd] = %s\n", line[fd]);
+		if (ft_strchr(line[fd], '\n'))
+			break;
 	}
 	return (line[fd]);
 }
 
-char	*get_line(int fd, char **line)
+char	*get_line(char *line)
 {
-	printf("Inside get_line %d, %p\n", fd, line);
-	return ("");
+	//char	*buffer = "Razan\nSrour";
+	char	*newline_pos;
+	//size_t	len;
+	
+	printf("passed line: %s\n", line);
+	newline_pos = ft_strchr(line, '\n');
+	printf("newline: %s\n", newline_pos);
+	return (newline_pos);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
+	while(*s != '\0' && *s != (unsigned char)c)
 		s++;
-	}
-	if (c == '\0')
+	if (*s == (unsigned char)c)
 		return ((char *)s);
 	return (NULL);
 }
