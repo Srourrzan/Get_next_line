db2fa1259c24aaff60305702803c15c2e6179ed8
Razan Srour <razanmsrour@gmail.com>
2024-11-11 19:46:47 +0300

    Adding the bonus part

---
+++ get_next_line_bonus.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 18:49:48 by gitpod            #+#    #+#             */
+/*   Updated: 2024/11/11 19:44:31 by rsrour           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*line[FD_MAX];
+	char		*p_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	line[fd] = manage_buffer(fd, line[fd]);
+	if (line[fd] == NULL)
+		return (NULL);
+	p_line = ft_get_line(line[fd]);
+	line[fd] = get_rest_line(line[fd]);
+	return (p_line);
+}
+
+char	*manage_buffer(int fd, char *line)
+{
+	char	*temp;
+	ssize_t	read_size;
+
+	read_size = 1;
+	temp = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!temp)
+	{
+		free(line);
+		return (NULL);
+	}
+	while (!ft_find_newline(line) && read_size != 0)
+	{
+		read_size = read(fd, temp, BUFFER_SIZE);
+		if (read_size == -1)
+		{
+			free(temp);
+			free(line);
+			return (NULL);
+		}
+		temp[read_size] = '\0';
+		line = ft_strjoin(line, temp);
+	}
+	free(temp);
+	return (line);
+}
+
+char	*ft_get_line(char *line)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (!line[i])
+		return (NULL);
+	while (line[i] && line[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (line[i] && line[i] != '\n')
+	{
+		str[i] = line[i];
+		i++;
+	}
+	if (line[i] == '\n')
+	{
+		str[i] = line[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 18:50:07 by gitpod            #+#    #+#             */
+/*   Updated: 2024/11/11 19:45:04 by rsrour           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_strlen(char *str)
+{
+	size_t	iter;
+	if(!str && !*str)
+		return (-1);
+	iter = 0;
+	while (str[iter])
+		iter++;
+	return (iter);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = -1;
+	j = 0;
+	if (!s1)
+	{
+		s1 = (char *)malloc(sizeof(char) * 1);
+		if(!s1)
+		{
+			return (NULL);
+		}
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[++i] != '\0')
+		str[i] = s1[i];
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_find_newline(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char) '\n')
+			return ((char *) &s[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*get_rest_line(char *line)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (!line[i])
+	{
+		free(line);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(line) - i));
+	if (!str)
+	{
+		free(line);
+		return (NULL);
+	}
+	i++;
+	while (line[i])
+		str[j++] = line[i++];
+	str[j] = '\0';
+	free(line);
+	return (str);
+}
